a87f43d6e8c0f34869c6f1640a4dada18c50181e
Jellewijma <jelle.wijma@gmail.com>
2020-12-24 16:37:32 +0100

    overhaul - V0.5.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,10 @@
 var menu = true;
 function openNav() {
   if (menu == true) {
-    document.getElementById("aside").style.transform = "translate(0, 0)";
+    document.getElementById("menu").style.transform = "translate(0, 0)";
     menu = false;
   } else if (menu == false) {
-    document.getElementById("aside").style.transform = "translate(100vw, 0)";
+    document.getElementById("menu").style.transform = "translate(100vw, 0)";
     menu = true;
   }
 }
